9d9549e44ce390e606d82bc53076c88220090244
Evgenii_Lobachev <lobachev9614@gmail.com>
2025-11-16 15:59:05 +0300

    add reading_from_json.py and update README.md

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,6 +1,6 @@
 def test_init_category(category_bakery, category_fpv):
-    assert category_bakery.name == 'bakery'
-    assert category_bakery.description == 'bakery'
+    assert category_bakery.name == "bakery"
+    assert category_bakery.description == "bakery"
     assert len(category_bakery.products) == 2
 
     assert category_bakery.category_count == 2
@@ -8,4 +8,3 @@ def test_init_category(category_bakery, category_fpv):
 
     assert category_bakery.product_count == 4
     assert category_fpv.product_count == 4
-
